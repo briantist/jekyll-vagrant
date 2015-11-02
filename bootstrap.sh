9977c2abd0e6485569209800c64d970072945277
Brian Scholer <brian@briantist.com>
2015-11-01 19:29:57 -0500

    'enviro'

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,18 @@ apt-get -y install build-essential
 apt-get -y install ruby2.1
 apt-get -y install ruby2.1-dev
 
+# other stuff
+apt-get -y install git
+
+#timezone
+echo America/New_York > /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+# exitwp
+apt-get -y install python-yaml
+apt-get -y install python-bs4
+apt-get -y install python-html2text
+
 #install github pages requirements (jekyll)
 sudo gem update --system
 sudo gem install github-pages
